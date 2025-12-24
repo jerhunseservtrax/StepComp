ce3032676941292b409ff1e6d73cd9ccb7af2a16
Jeffery <Jerhunse1@gmail.com>
2025-12-24 13:31:07 -0600

    Initial Commit

---
+++ StepComp/StepCompApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepCompApp.swift
+//  StepComp
+//
+//  Created by Jeffery Erhunse on 12/24/25.
+//
+
+import SwiftUI
+
+@main
+struct StepCompApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
